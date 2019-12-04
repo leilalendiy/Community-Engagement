d7d2e4906f420682c07d226318b68bbb5b97857d
Leila <leilalendiy@users.noreply.github.com>
2019-12-04 14:59:22 -0500

    Final update

--- src/app/africa/africa.component.ts
+++ src/app/africa/africa.component.ts
@@ -10,12 +10,18 @@ export class AfricaComponent implements OnInit {
 
   constructor() {
     this.items = [
-      {instrument: 'Trumpet', image: 'assets/images/trumpet.png', sound: 'assets/sounds/trumpet.wav',
-       desc: "Well, this is gonna be a very very very long paragraph explaining the instruments and whatnot"},
-      {instrument: 'Violin', image: 'assets/images/violin.png', sound: 'assets/sounds/violin.mp3',
-       desc: "Well, this is gonna be a very very very long paragraph explaining the instruments and whatnot"},
-      {instrument: 'Cat', image: 'assets/images/cat.png', sound: 'assets/sounds/mockingbird.wav',
-       desc: "Well, this is gonna be a very very very long paragraph explaining the instruments and whatnot"},
+      {instrument: 'Mbira Dzavadzimu', image: 'assets/images/mbira.png', sound: 'assets/sounds/mbira.mp3',
+       desc: "Traditional to the Shona People of Zimbabwe, the Mbira Dzavadzimu is an instrument consisting of a wooden board (typically equipped with a resonator) with attached staggered metal tines. The instrument is played by holding it in one’s hand and plucking the tines with the right forefinger, the thumbs, and sometimes the left forefinger, classifying it as a lamellophone. "},
+      {instrument: 'Kora', image: 'assets/images/kora.png', sound: 'assets/sounds/kora.mp3',
+       desc: "A Mandinka harp used extensively in West Africa that is built from a small calabash cut in half and covered with cow skin to make a resonator with a long hardwood neck. It has 21 strings, each playing a different note. Traditionally, Kora players come from jail families who are storytellers, genealogists, and historians who pass their skills on to their descendants."},
+      {instrument: 'Tambin', image: 'assets/images/tambin.png', sound: 'assets/sounds/tambin.mp3',
+       desc: "Also known as the Fula Flute, the Tambin is a diagonal diatonic flute without a bell, made from a conical vine, with three finger-holes and a rectangular embouchure with two wings on either side. The Tambin is recognized as the national instrument of the West African Fula. The tambin has been said to bring “tears with its haunting sounds and melodies that reach deep inside one's soul.” The flute was featured in the 2019 Marvel movie Black Panther."},
+       {instrument: 'Djembe', image: 'assets/images/djembe.png', sound: 'assets/sounds/djembe.mp3',
+       desc: " Originating from West Africa, a djembe is a rope-tuned skin-covered goblet drum played with bare hands. The name djembe originates from the Bambara people’s(from Mali) saying 'Anke djé, anke bé' which translates to 'everyone gather together in peace' and defines the drum's purpose. Traditionally, the djembe is played only by men along with the dunun that always accompany the djembe."},
+       {instrument: 'Shekere', image: 'assets/images/shekere.png', sound: 'assets/sounds/shekere.mp3',
+       desc: "A West African percussion instrument consisting of a dried gourd with beads or cowries woven into a net covering the gourd. The shaker is made from vine gourds that grow on the ground. The sound of the instrument changes depending on the shape of the gourd. In West Africa, percussion instruments like the Shekere are traditionally played by women."},
+       {instrument: 'Ngoni', image: 'assets/images/ngoni.png', sound: 'assets/sounds/ngoni.mp3',
+       desc: "A West African string instrument that has a body made of wood or calabash with dried animal skin head stretched over it. The ngoni is capable of producing fast melodies. Traditionally, the ngoni is played to celebrate special occasions and ceremonies. The ngoni is known to have existed since at least 1352, and it is believed that the American banjo is an evolved form of the ngoni after Mande slaves were exported there."},
     ]
   }
 
--- src/app/asia/asia.component.ts
+++ src/app/asia/asia.component.ts
@@ -10,18 +10,18 @@ export class AsiaComponent implements OnInit {
 
   constructor() {
     this.items = [
-      {instrument: 'Dizi', image: 'assets/images/dizi.png', sound: 'assets/sounds/trumpet.wav',
-       desc: "Dizi is a Chinese flute that is traditionally made of one piece of bamboo tube. Nowadays, dizi can be made of different types of bamboo tubes for various sizes and keys. The two types of dizi are qudi, the longer version from southern China, and bangdi, the shorter version from northern China."},
-      {instrument: 'Erhu', image: 'assets/images/erhu.png', sound: 'assets/sounds/violin.mp3',
-       desc: "Erhu is a Chinese chordophone with two tuning pegs. It is a main accompanying instrument for Chinese opera performances and instrumental ensembles. Historically known as \"huqin\", meaning barbarian's fiddle, the erhu is held vertically on the performer's left thigh."},
-      {instrument: 'Kalaleng', image: 'assets/images/kalaleng.png', sound: 'assets/sounds/mockingbird.wav',
-       desc: "Kalaleng is a Filipino nose flute with a range of two and a half octaves. By overblowing, it can play different harmonics. Kalaleng, or \"tongali\", has a narrow internal diameter and is traditionally made of bamboo tube. To increase the airflow through the flute, players can plug the other nostril."},
-      {instrument: 'Koto', image: 'assets/images/koto.png', sound: 'assets/sounds/trumpet.wav',
-       desc: "Koto is a Japanese zither that has 13 silk strings and movable bridges. It is played by plucking the strings using the thumb and first two fingers of the right hand. Both solo and chamber music use the koto and in the latter form, the performer generally also sings. It is also known as \"kin\"."},
-      {instrument: 'Kubing', image: 'assets/images/kubing.png', sound: 'assets/sounds/violin.mp3',
-       desc: "Kubing is a Filipino jaw harp that is mainly used for courtship and children's recreation. It is played by flexing and releasing the tip of the frame, which is held in the left hand and pressed against a player's lips. It is made of bamboo and produces a fixed pitch that is rich in overtones."},
-      {instrument: 'Taiko', image: 'assets/images/taikodrum.png', sound: 'assets/sounds/mockingbird.wav',
-        desc: "Taiko is the Japanese word for \"drum\". Taiko drums are classified based on the methods they were made as well as the different sizes of woods, lengths of ropes, and types of animal hides used for the drum skins. They are played on a slanted stand with two wooden sticks, or \"bachi\"."},
+      {instrument: 'Dizi', image: 'assets/images/dizi.png', sound: 'assets/sounds/dizi.mp3',
+       desc: "Dizi is a Chinese flute that is traditionally made of one piece of bamboo tube. Nowadays, dizi can be made of different types of bamboo tubes for various sizes and keys. The two types of dizi are qudi, the longer version from southern China, and bangdi, the shorter version from northern China. The qudi has a darker tone whereas the bangdi has a brighter tone."},
+      {instrument: 'Erhu', image: 'assets/images/erhu.png', sound: 'assets/sounds/erhu.mp3',
+       desc: "Erhu is a Chinese chordophone with two tuning pegs. It is a main accompanying instrument for Chinese opera performances and instrumental ensembles. Historically known as \"huqin\", meaning barbarian's fiddle, the erhu is held vertically on the performer's left thigh with the sound table facing to the right."},
+      {instrument: 'Kalaleng', image: 'assets/images/kalaleng.png', sound: 'assets/sounds/kalaleng.mp3',
+       desc: "Kalaleng is a Filipino nose flute with a range of two and a half octaves. By overblowing, it can play different harmonics. Kalaleng, or \"tongali\", has a narrow internal diameter and is traditionally made of bamboo tube. To increase the airflow through the flute, players can plug the other nostril. The kalaleng can change length to play various scales."},
+      {instrument: 'Koto', image: 'assets/images/koto.png', sound: 'assets/sounds/koto.mp3',
+       desc: "Koto is a Japanese zither that has 13 silk strings and movable bridges. It is played by plucking the strings using the thumb and first two fingers of the right hand. Both solo and chamber music use the koto and in the latter form, the performer generally also sings. It is also known as \"kin\" and is related to the Korean instrument called gayageum."},
+      {instrument: 'Kubing', image: 'assets/images/kubing.png', sound: 'assets/sounds/kubing.mp3',
+       desc: "Kubing is a Filipino jaw harp that is mainly used for courtship and children's recreation. It is played by flexing and releasing the tip of the frame, which is held in the left hand and pressed against a player's lips. It is made of bamboo and produces a fixed pitch that is rich in overtones. The kubing is often used among Muslim communities in the Mindanao island."},
+      {instrument: 'Taiko', image: 'assets/images/taikodrum.png', sound: 'assets/sounds/taiko.mp3',
+        desc: "Taiko is the Japanese word for \"drum\". Taiko drums are classified based on the methods they were made as well as the different sizes of woods, lengths of ropes, and types of animal hides used for the drum skins. They are played on a slanted stand with two wooden sticks, or \"bachi\". Smaller taiko drums can be carried on a strap across the player's shoulder."},
     ]
   }
 
